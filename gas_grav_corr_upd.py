1804be3f0b4cf882782c64e195093dc739792f62
MICHAEL OSARUCHI <129209068+MichaelOsaruchi@users.noreply.github.com>
2023-05-15 08:58:15 +0100

    Update gas_grav_corr_upd.py

--- gas_grav_corr_upd.py
+++ gas_grav_corr_upd.py
@@ -19,7 +19,7 @@ gas_gravity = float(gas_gravity)
 if co2_comp <= 0.12 or n2_comp <= 0.03 or h2s_comp <= 0:
    gas_gravity = (gas_gravity - (1.1767*h2s_comp) - (1.5196*co2_comp) - (0.9672*n2_comp) - \
                    (0.622*h2o_comp))/(1- h2s_comp - co2_comp - n2_comp - h2o_comp)
-    print('The corrected gas gravity is', gas_gravity)
+   print('The corrected gas gravity is', gas_gravity)
 
 # continuing after the if block
 
